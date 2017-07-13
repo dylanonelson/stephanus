264e74a2a2e888c5aa13912d353176034fdc36f8
Dylan Nelson <Dylan.Nelson@nytimes.com>
2017-07-13 13:13:50 -0400

    Add example

--- api/schema/validate.py
+++ api/schema/validate.py
@@ -2,14 +2,22 @@ from jsonschema import validate
 from termcolor import cprint
 import json
 
-with open('./stephanus_page.json') as file:
-    schema = json.load(file)
+examples = []
 
-with open('./examples/327a.json') as file:
-    example = json.load(file)
+with open('./stephanus_page.json') as f:
+    schema = json.load(f)
+
+with open('./examples/327a.json') as f:
+    p327a = json.load(f)
+    examples.append(p327a)
+
+with open('./examples/327c.json') as f:
+    p327c = json.load(f)
+    examples.append(p327c)
 
 try:
-    validate(example, schema)
+    for ex in examples:
+        validate(ex, schema)
     cprint ('VALID!', 'green')
 except Exception as e:
     cprint(e, 'red')
