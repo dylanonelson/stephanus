805c4038cd7d2d5dd3d784987a5064f0cd091bf3
Dylan Nelson <Dylan.Nelson@nytimes.com>
2017-07-13 13:35:52 -0400

    Refactor validate script

--- api/schema/validate.py
+++ api/schema/validate.py
@@ -2,22 +2,21 @@ from jsonschema import validate
 from termcolor import cprint
 import json
 
-examples = []
-
 with open('./stephanus_page.json') as f:
     schema = json.load(f)
 
-with open('./examples/327a.json') as f:
-    p327a = json.load(f)
-    examples.append(p327a)
-
-with open('./examples/327c.json') as f:
-    p327c = json.load(f)
-    examples.append(p327c)
+examples = [
+    './examples/327a.json',
+    './examples/327c.json',
+]
 
 try:
-    for ex in examples:
-        validate(ex, schema)
+    for example in examples:
+        with open(example) as f:
+            p = json.load(f)
+            validate(p, schema)
+
     cprint ('VALID!', 'green')
+
 except Exception as e:
     cprint(e, 'red')
