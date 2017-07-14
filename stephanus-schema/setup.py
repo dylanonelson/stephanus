335098ce4e36239ef55cdb1c316acc64cb82914b
Dylan Nelson <Dylan.Nelson@nytimes.com>
2017-07-14 16:53:04 -0400

    Reorganize

---
+++ stephanus-schema/setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Stephanus Schema',
+      version='0.1dev',
+      description='JSON Schema for Platonic texts',
+      author='Dylan Nelson',
+      author_email='dylanonelson@gmail.com'
+      packages=['schema'],
+     )
