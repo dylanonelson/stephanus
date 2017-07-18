5dadb99871712b156630c96559fc7e4edb86c71a
Dylan Nelson <Dylan.Nelson@nytimes.com>
2017-07-17 21:02:05 -0400

    Add script for install dependencies

--- stephanus-schema/manage.py
+++ stephanus-schema/manage.py
@@ -1,11 +1,34 @@
 from manager import Manager
+from subprocess import call, Popen, PIPE
+import os
+import pip
 import pytest
 
 manager = Manager()
 
 @manager.command
+def watch():
+    call('ptw')
+
+@manager.command
 def test():
     pytest.main()
+
+@manager.command
+def install(pkg):
+    dir = os.path.dirname(__file__)
+
+    reqs_to_frz = os.path.join(dir, 'requirements-to-freeze.txt')
+    with open(reqs_to_frz, 'a') as f:
+        f.write(pkg)
+
+    pip.main(['install', '-r', reqs_to_frz])
+    reqs = os.path.join(dir, 'requirements.txt')
+    p = Popen(['pip', 'freeze', '-r', reqs_to_frz], stdout=PIPE)
+    (out, err) = p.communicate()
+
+    with open(reqs, 'w') as f:
+        f.write(out.decode('utf-8'))
 
 if __name__ == '__main__':
     manager.main()
