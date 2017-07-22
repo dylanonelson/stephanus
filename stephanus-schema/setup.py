a81c6e5e504f66383f6b8c7532e088a958e623cc
dylanonelson <dylanonelson@gmail.com>
2017-07-22 14:55:00 -0400

    Fix syntax error

--- stephanus-schema/setup.py
+++ stephanus-schema/setup.py
@@ -6,6 +6,6 @@ setup(name='Stephanus Schema',
       version='0.1dev',
       description='JSON Schema for Platonic texts',
       author='Dylan Nelson',
-      author_email='dylanonelson@gmail.com'
+      author_email='dylanonelson@gmail.com',
       packages=['stephanus_schema'],
      )
