d37cbbc80ffbe27eefd1276d909176a6042273cb
dylanonelson <dylanonelson@gmail.com>
2017-07-22 17:53:31 -0400

    Add route for individual pages

--- stephanus-api/app/__init__.py
+++ stephanus-api/app/__init__.py
@@ -1,5 +1,5 @@
 #!flask/bin/python
-from flask import Flask
+from flask import Flask, jsonify
 from app.mongo import db
 
 app = Flask(__name__)
@@ -7,6 +7,13 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return "Hello, world!"
+
+@app.route('/<page>')
+def page(page):
+    match = db.pages.find_one({'id': page}, {'_id': 0})
+    resp = jsonify(match)
+    resp.status_code = 200
+    return resp
 
 if __name__ == '__main__':
     app.run(debug=True)
--- stephanus-api/run.py
+++ stephanus-api/run.py
@@ -1,3 +1,3 @@
-import app
+from app import app
 
-app.run()
+app.run(debug=True)
