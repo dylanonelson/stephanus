414c1b85d35b354f8e325d5ebbed3089dca24b9a
dylanonelson <dylanonelson@gmail.com>
2017-07-22 21:01:54 -0400

    Add validation of query params

--- stephanus-api/app/__init__.py
+++ stephanus-api/app/__init__.py
@@ -1,5 +1,8 @@
 #!flask/bin/python
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from webargs import fields
+from webargs.core import Parser
+from webargs.flaskparser import use_args
 import os
 
 app = Flask(__name__)
@@ -8,11 +11,24 @@ app_env = os.environ.get('PYTHON_ENV')
 app_env = app_env if app_env != None else 'development'
 app.config.from_object(f"app.config.{app_env}")
 
+from .validators import *
 from app.mongo import db
 
+index_args = {'from': fields.Str(required=True, validate=validate_min_page),
+              'to': fields.Str(required=True, validate=validate_max_page)}
+
 @app.route('/')
-def index():
-    return "Hello, world!"
+@use_args(index_args)
+def index(args):
+    from_page = args.get('from')
+    to_page = args.get('to')
+    query = {'$where': f"(this.id >= '{from_page}' && this.id <= '{to_page}')"}
+    matches = db.pages.find(query, { '_id': 0 })
+    list = []
+    for match in matches:
+        list.append(match)
+    resp = jsonify(list)
+    return resp
 
 @app.route('/<page>')
 def page(page):
@@ -20,6 +36,3 @@ def page(page):
     resp = jsonify(match)
     resp.status_code = 200
     return resp
-
-if __name__ == '__main__':
-    app.run(debug=True)
